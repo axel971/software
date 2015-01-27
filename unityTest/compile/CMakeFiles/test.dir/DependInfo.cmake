317df77115d62616a3b9a43a11c63498da4a8836
axel <axel.largent@hotmail.fr>
2015-01-27 20:00:13 +0100

    rename test and unityTest

---
+++ unityTest/compile/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/axel/Documents/code/software/source/detector/DOGDetector.cpp" "/home/axel/Documents/code/software/test/compile/CMakeFiles/test.dir/home/axel/Documents/code/software/source/detector/DOGDetector.cpp.o"
+  "/home/axel/Documents/code/software/source/detector/Feature.cpp" "/home/axel/Documents/code/software/test/compile/CMakeFiles/test.dir/home/axel/Documents/code/software/source/detector/Feature.cpp.o"
+  "/home/axel/Documents/code/software/source/imagePyramid/DOGPyramid.cpp" "/home/axel/Documents/code/software/test/compile/CMakeFiles/test.dir/home/axel/Documents/code/software/source/imagePyramid/DOGPyramid.cpp.o"
+  "/home/axel/Documents/code/software/source/imagePyramid/GaussianPyramid.cpp" "/home/axel/Documents/code/software/test/compile/CMakeFiles/test.dir/home/axel/Documents/code/software/source/imagePyramid/GaussianPyramid.cpp.o"
+  "/home/axel/Documents/code/software/source/imagePyramid/LevelPyramid.cpp" "/home/axel/Documents/code/software/test/compile/CMakeFiles/test.dir/home/axel/Documents/code/software/source/imagePyramid/LevelPyramid.cpp.o"
+  "/home/axel/Documents/code/software/source/imagePyramid/Pyramid.cpp" "/home/axel/Documents/code/software/test/compile/CMakeFiles/test.dir/home/axel/Documents/code/software/source/imagePyramid/Pyramid.cpp.o"
+  "/home/axel/Documents/code/software/test/source/main.cpp" "/home/axel/Documents/code/software/test/compile/CMakeFiles/test.dir/source/main.cpp.o"
+  "/home/axel/Documents/code/software/test/source/testDetector/TestDOGDetector.cpp" "/home/axel/Documents/code/software/test/compile/CMakeFiles/test.dir/source/testDetector/TestDOGDetector.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
