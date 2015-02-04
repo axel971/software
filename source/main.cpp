eac614dd2c2fd17e6cef21b84e6527c69a5eeda1
axel <axel.largent@hotmail.fr>
2015-02-04 21:30:10 +0100

    add main

--- source/main.cpp
+++ source/main.cpp
@@ -8,27 +8,12 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-  /*
+
   QApplication app(argc,argv);
 
   Controller controller;
   controller.showView();
 
   return app.exec();
-  */
-
-  Mat image;
-  image = imread("lena.jpg", CV_LOAD_IMAGE_COLOR);
-
-  DOGDetector DOGDetector(image, 3, 5, sqrt(2), 1.6);
-  DOGDetector();
-  cout << DOGDetector.getNumbersFeatures() << endl;
-
-  //construct pyramide
-  //GaussianPyramid pyramid(image, 3, 5, sqrt(2), 1.6);  
-  //pyramid.build();
- 
-
-  return 0;
 
 }
