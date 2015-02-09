8cffb8c920110718c1e5ac6ef0f227515dd0fd56
axel <axel.largent@hotmail.fr>
2015-02-09 08:39:19 +0100

    Refactoring

--- source/main.cpp
+++ source/main.cpp
@@ -18,15 +18,12 @@ int main(int argc, char** argv)
   */
 
   Mat image;
-  image = imread("lena.jpg", CV_LOAD_IMAGE_COLOR);
+  image = imread("../data/lena.jpg", CV_LOAD_IMAGE_COLOR);
 
   DOGDetector DOGDetector(image, 3, 5, 1.6);
   DOGDetector();
   cout << DOGDetector.getNumbersFeatures() << endl;
 
-  DOGDetector();
-  cout << DOGDetector.getNumbersFeatures() << endl;
-  
   //construct pyramide
   //GaussianPyramid pyramid(image, 3, 5, sqrt(2), 1.6);  
   //pyramid.build();
